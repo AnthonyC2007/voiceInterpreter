9f996486320349d024a99f4bd1c2043fabb091d0
Adam Buchowski <adam.buchowski@proton.me>
2026-04-20 22:30:24 +0100

    We need only log_mel and labels

--- extract_mfcc.py
+++ extract_mfcc.py
@@ -20,25 +20,27 @@ def fix_length(x, max_len):
     return x
 
 def extract_features(audio, n_mfcc=13, n_mels=64, hop_length=256, max_len=300):
-    mfcc = librosa.feature.mfcc(y=audio, sr=SAMPLE_RATE, n_mfcc=n_mfcc, n_mels=n_mels, hop_length=hop_length)
+    #mfcc = librosa.feature.mfcc(y=audio, sr=SAMPLE_RATE, n_mfcc=n_mfcc, n_mels=n_mels, hop_length=hop_length)
 
-    delta = librosa.feature.delta(mfcc)
+    #delta = librosa.feature.delta(mfcc)
 
-    mfcc = mfcc.T
-    delta = delta.T
+    #mfcc = mfcc.T
+    #delta = delta.T
 
     mel = librosa.feature.melspectrogram(y=audio, sr=SAMPLE_RATE, n_mels=n_mels, hop_length=hop_length)
     log_mel = librosa.power_to_db(mel).T
     
-    mfcc = fix_length(mfcc, max_len)
-    delta = fix_length(delta, max_len)
+    #mfcc = fix_length(mfcc, max_len)
+    #delta = fix_length(delta, max_len)
     log_mel = fix_length(log_mel, max_len)
 
-    return {
-        "mfcc": mfcc.astype(np.float32),        # (T, 13)
-        "delta": delta.astype(np.float32),      # (T, 13)
-        "log_mel": log_mel.astype(np.float32)   # (T, 64)
-    }
+    # return {
+    #     "mfcc": mfcc.astype(np.float32),        # (T, 13)
+    #     "delta": delta.astype(np.float32),      # (T, 13)
+    #     "log_mel": log_mel.astype(np.float32)   # (T, 64)
+    # }
+
+    return {"log_mel": log_mel.astype(np.float32)} 
 
 # Audio object is with metadata. We only need the raw audio array.
 def extract_raw_audio(sample):
@@ -59,7 +61,7 @@ if __name__ == "__main__":
     dataset = dataset["train"].remove_columns(["source"])
     dataset = dataset.cast_column("audio", Audio(sampling_rate = SAMPLE_RATE))
     dataset = dataset.map(extract_raw_audio, num_proc = cpu_count())
-    df: pd.DataFrame = dataset.to_pandas()
+    df: pd.DataFrame = dataset.to_pandas() # type: ignore
     df["labels"] = df["labels"].str.replace("female_", "", regex=False).str.replace("male_", "", regex=False)
     df["labels"] = df["labels"].map(classes).values
 
@@ -75,15 +77,16 @@ if __name__ == "__main__":
         features = extract_features(row["audio"])
 
         if features is not None:
-            mfcc_list.append(features["mfcc"])
-            delta_list.append(features["delta"])
+            #mfcc_list.append(features["mfcc"])
+            #delta_list.append(features["delta"])
             log_mel_list.append(features["log_mel"])
             labels_list.append(row["labels"])
 
-    mfcc_array = np.stack(mfcc_list)
-    delta_array = np.stack(delta_list)
+    #mfcc_array = np.stack(mfcc_list)
+    #delta_array = np.stack(delta_list)
     log_mel_array = np.stack(log_mel_list)
     labels_array = np.array(labels_list)
 
-    np.savez("data/features_mfcc.npz", mfcc=mfcc_array, delta=delta_array, log_mel=log_mel_array, labels=labels_array)
-    print("Feature extraction completed and saved to features_mfcc.npz")
+    #np.savez("data/features_mfcc.npz", mfcc=mfcc_array, delta=delta_array, log_mel=log_mel_array, labels=labels_array)
+    np.savez("data/features_log_mel.npz", log_mel=log_mel_array, labels=labels_array)
+    print("Feature extraction completed and saved to features_log_mel.npz")
